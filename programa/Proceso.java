a73c3e0c854947957ce4719686d61e4c3f6af76a
Sebastian García <estuardosebastian10@gmail.com>
2025-10-06 18:12:59 -0600

    Completar lógica de Proceso y ProcesoInvalidoException

--- programa/Proceso.java
+++ programa/Proceso.java
@@ -1,1 +1,36 @@
+// @author Sebastian_Garcia
+// @version 1.0
+// @fecha 06/10/2025
+// Descripción: Clase abstracta para todos los procesos del sistema
+
+public abstract class Proceso {
+    // ATRIBUTOS
+    protected int procesoId;
+    protected String nombre;
+
+    // CONSTRUCTOR
+    public Proceso(int procesoId, String nombre){
+        this.procesoId = procesoId;
+        this.nombre = nombre;
+    }
+
+    // MÉTODO: para obligar que las subclases tengan que implementarlo
+    public abstract String ejecutar();
+
+    // GETTERS
+    public int getProcesoId(){
+        return procesoId;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    // SOBREESCRIBIR: para obtener información importante del proceso
+    @Override
+    public String toString(){
+        String tipo = this.getClass().getSimpleName();
+        return "PROCESO ID: " + procesoId + ", NOMBRE: " + nombre + ", TIPO: " + tipo;
+    }
+}
+
 
--- programa/ProcesoInvalidoException.java
+++ programa/ProcesoInvalidoException.java
@@ -1,3 +1,10 @@
-public class ProcesoInvalidoException {
-    
+// @author Sebastian_Garcia
+// @version 1.0
+// @fecha 06/10/2025
+// Descripción: Clase para manejar los errores de forma personalizada al crear un proceso.
+
+public class ProcesoInvalidoException extends Exception {
+    public ProcesoInvalidoException(String message){
+        super(message); // se envía el mensaje de error a la superclase
+    }
 }
